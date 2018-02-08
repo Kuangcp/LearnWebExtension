a2210f8a98e4cfb3c5e39e03a66c777f030f0741
mythos <kuang.myth@foxmail.com>
2018-02-08 14:04:03 +0800

    add json file

--- mythos-navigation/js/home.js
+++ mythos-navigation/js/home.js
@@ -1,45 +1,55 @@
 
 
-  var fun = 0        
-  $(".menu-element").on('click', function () {
-      type = $(this).data('type')
-      // console.log('click', type)
+var fun = 0        
+$(".menu-element").on('click', function () {
+    type = $(this).data('type')
+    // console.log('click', type)
 
-      if (type == 'fun') {
-          fun++
-      }else{
-          fun = 0                
-      }
-      if (type == 'fun') {
-          insertData(list.fun)
-      } else if (type == 'code') {
-          insertData(list.code)
-      } else if (type == 'cloud') {
-          insertData(list.cloud)
-      } else if (type == 'read') {
-          insertData(list.read)
-      }
-      if(fun>2){
-          window.location = 'refreshData.html'
-      }
-  })
+    if (type == 'fun') {
+        fun++
+    }else{
+        fun = 0                
+    }
+    if (type == 'fun') {
+        insertData(list.fun)
+    } else if (type == 'code') {
+        insertData(list.code)
+    } else if (type == 'cloud') {
+        insertData(list.cloud)
+    } else if (type == 'read') {
+        insertData(list.read)
+    }
+    if(fun>2){
+        window.location = 'refreshData.html'
+    }
+})
+function initData(config, list){
+    config.type.forEach(function(urlType){
+        console.log(urlType)
+        list[urlType].forEach(function(url){
+            console.log(url)
+        })
+    })
 
-  function insertData(lists) {
-      temp = '<div class="row">'
-      count = 0
-      lists.forEach(function (value) {
-          count++
-          // console.log(value.title, value.url)
-          temp += '<div class="col-md-' + config.colNum + '" style="width=50%;">' +
-              '<a href="' + value.url + '" style="text-decoration: none;">' +
-              '<div class="box"><div class="text">' + value.title + '</div></div></div></a>'
-          if (count % config.col == 0) {
-              temp += '</div><div class="row">'
-          }
-      })
-      temp += '</div>'
-      $("#main").html(temp)
-  }
-  $(function () {
-      insertData(list.code)
-  });
+}
+function insertData(lists) {
+    temp = '<div class="row">'
+    count = 0
+    lists.forEach(function (value) {
+        count++
+        // console.log(value.title, value.url)
+        temp += '<div class="col-md-' + config.colNum + '" style="width=50%;">' +
+            '<a href="' + value.url + '" style="text-decoration: none;">' +
+            '<div class="box"><div class="text">' + value.title + '</div></div></div></a>'
+        if (count % config.col == 0) {
+            temp += '</div><div class="row">'
+        }
+    })
+    temp += '</div>'
+    $("#main").html(temp)
+}
+
+$(function () {
+    insertData(list.code)
+    initData(config, list)
+});
